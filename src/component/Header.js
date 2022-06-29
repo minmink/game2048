34170f1216ebdb9e6c1b9621c835b191d83cf10a
minmink <ahnmink004@gmail.com>
2022-06-29 10:07:09 +0900

    game 2048 by react

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className="heading">
+          <h1 className="title">2048</h1>
+          <div className="scores-container">
+            <div className="score-container">0</div>
+            <div className="best-container">2480</div>
+          </div>
+        </header>
+      );
+}
