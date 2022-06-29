4f8655f8d87f6859de65e8d1db7e3632485674cd
minmink <ahnmink004@gmail.com>
2022-06-29 10:17:36 +0900

    score 처리(reduce)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AboveGame from './component/AboveGame';
 import Game from './component/Game';
 import Header from './component/Header';
 
 export default function App() {
+  const [score, setScore] = useState(0);
   return (
     <div className="container">
-      <Header />
+      <Header score={score} bestScore={0} />
       <AboveGame />
-      <Game />
+      <Game setScore={setScore} />
     </div>
   );
 }
